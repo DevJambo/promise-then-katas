4863330f9cbe9b6437e33950d7bf714c8120eb48
James Goodman <jamesgoodman206@gmail.com>
2022-12-07 19:22:29 +0000

    first commit, finished the last two tests

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -35,7 +35,12 @@ const cat = () => {};
 // 3 Create a function that uses the fetch function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
-const dog = () => {};
+const dog = async() => {
+    const response = await fetch('dogs')
+    const naughtiestDog = response.data.dogs.reduce( (previousDog, currentDog)=> previousDog.naughty > currentDog.naughty ? previousDog : currentDog)
+
+    return naughtiestDog;
+};
 
 // 4 Create a function that uses the fetch function to make requests to the "jokes" URL and returns
 // a joke object with the key of question and answer - expected return { 
@@ -47,7 +52,14 @@ const dog = () => {};
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
 //
 
-const joke = () => {};
+const joke = async() => {
+    const questionResponse = await fetch('jokes','question')
+    const answerResponse = await fetch('jokes')
+    return{ 
+        question: questionResponse.joke,
+        answer: answerResponse.answer 
+    }
+};
 
 module.exports = {
     food,
